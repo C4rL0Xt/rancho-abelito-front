0bf07648aa478458f562954e6687b06b2fae78eb
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2024-12-22 23:27:15 -0500

    header v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Rancho-Abelito-Front';
+  title = 'El Rancho de Abelito';
 }
